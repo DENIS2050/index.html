f8ad6c5116d592d20fb9f403b6b41f3798926e2f
DENIS2050 <96338795+DENIS2050@users.noreply.github.com>
2021-12-19 13:32:11 +0100

    Add files via upload

---
+++ ZD-oceny 1.py
@@ -0,0 +1,25 @@
+print('Oceny')
+
+def isBool(verUser):
+    if (varUser == 'tak'):
+        return True
+    elif(varUser == 'nie'):
+        return False
+    else:
+        return print('Zle wprowadzone dane !')
+
+userName = input('Podaj twoje imie')
+
+userData = {
+    'name':userName
+}
+
+userData = {userName}
+
+# print('Twoje imie' + userName[0])
+
+import random
+a = [1, 5, 1, 5,]
+b = random.choice(a)
+
+print(b)
